3181dc5f21e333e698ac9ab43b722f7830df5fc5
Leon <yanglyu.leon.7@gmail.com>
2019-06-26 10:57:53 -0700

    start: Getting started with Service Fabric
    fea: Creating an ASP.NET Web API service as a reliable service

---
+++ VotingWeb/Controllers/VotesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Query;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace VotingWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Votes")]
+    public class VotesController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public VotesController(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            List<KeyValuePair<string, int>> votes = new List<KeyValuePair<string, int>>();
+            votes.Add(new KeyValuePair<string, int>("Pizza", 3));
+            votes.Add(new KeyValuePair<string, int>("Ice Cream", 4));
+            return Json(votes);
+        }
+    }
+}
